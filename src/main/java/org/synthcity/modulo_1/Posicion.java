721256a1e4b1d1511511085bcd6d423e3e716d60
Víctor <victorosuna114@gmail.com>
2026-04-08 17:28:37 +0200

    Añadido constructor sobrecargado a clase Posicion

--- src/main/java/org/synthcity/modulo_1/Posicion.java
+++ src/main/java/org/synthcity/modulo_1/Posicion.java
@@ -2,6 +2,11 @@ public class Posicion {
     private int fila;
     private int columna;
 
+    public Posicion(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+    }
+
     public int getFila() {
         return fila;
     }
