a8fd20f08b6b69cec0476744be33fb3193305fd5
Víctor <victorosuna114@gmail.com>
2026-04-08 17:20:50 +0200

    Creacion clase Posicion + getters

---
+++ src/main/java/org/synthcity/modulo_1/Posicion.java
@@ -0,0 +1,12 @@
+public class Posicion {
+    private int fila;
+    private int columna;
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+}
