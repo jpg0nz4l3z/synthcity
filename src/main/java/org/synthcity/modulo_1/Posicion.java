173385289a5d764bd9e3e20de1548c5952d0b629
Víctor <victorosuna114@gmail.com>
2026-04-09 17:50:36 +0200

    Validaciones en constructor y modificación de métodos añadiendo equals, hashCode y toString

--- src/main/java/org/synthcity/modulo_1/Posicion.java
+++ src/main/java/org/synthcity/modulo_1/Posicion.java
@@ -1,8 +1,13 @@
+import java.util.Objects;
+
 public class Posicion {
     private int fila;
     private int columna;
 
     public Posicion(int fila, int columna) {
+        if (fila < 0 || columna < 0) {
+            throw new IllegalArgumentException("La fila y la columna deben ser >= 0");
+        }
         this.fila = fila;
         this.columna = columna;
     }
@@ -13,5 +18,28 @@ public class Posicion {
 
     public int getColumna() {
         return columna;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Posicion posicion = (Posicion) o;
+        return fila == posicion.fila && columna == posicion.columna;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fila, columna);
+    }
+
+    @Override
+    public String toString() {
+        return "Fila -> " + fila + ". Columna -> " + columna;
     }
 }
