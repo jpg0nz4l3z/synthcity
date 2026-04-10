2e6c74c6d26059b13751d45f98bc86e2d938764c
Víctor <victorosuna114@gmail.com>
2026-04-10 17:24:34 +0200

    Modificación final clase Ciudad Sprint 1

--- src/main/java/org/synthcity/modulo_1/Ciudad.java
+++ src/main/java/org/synthcity/modulo_1/Ciudad.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public abstract class Ciudad {
     private String nombre;
@@ -62,6 +64,99 @@ public abstract class Ciudad {
 
     public int capacidadMaxima() {
         return filas * columnas;
+    }
+
+    // Métodos complementarios
+
+    public void addBloque(Bloque bloque){
+
+        if (bloque == null){
+            throw new IllegalArgumentException("El Bloque no existe");
+        }
+        int fila=bloque.getFilas();
+        int columna= bloque.getColumnas();
+        if (fila < 0 || fila >= filas || columna < 0 || columna >= columnas ){
+            throw new IllegalArgumentException("La posición no se encuentra dentro del tablero");
+        }
+        if (tablero[fila][columna]!= null){
+            throw new IllegalArgumentException("La casilla ya está ocupada");
+        }
+        tablero[fila][columna] = bloque;
+    }
+
+    public void removeBloque(int fila, int columna){
+        if (fila < 0 || fila >= filas || columna < 0 || columna >= columnas ){
+            throw new IllegalArgumentException("La posición no se encuentra dentro del tablero");
+        }
+        if (tablero[fila][columna] == null){
+            throw new IllegalArgumentException("No Hay un bloque que eliminar en esta posición");
+        }
+        tablero[fila][columna] = null;
+    }
+
+    public List<Bloque> listarBloques() {
+        List<Bloque> listaBloques = new ArrayList<>();
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (tablero[i][j] != null) {
+                    listaBloques.add(tablero[i][j]);
+                }
+            }
+        }
+        return listaBloques;
+    }
+
+    public List<Bloque> listarBloquesActivos(){
+        List<Bloque> listaBloquesActivos = new ArrayList<>();
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (tablero[i][j] != null && tablero[i][j].estaActivo()) {
+                    listaBloquesActivos.add(tablero[i][j]);
+                }
+            }
+        }
+        return listaBloquesActivos;
+    }
+
+    public int contarBloques(){
+        int contadorBloques=0;
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (tablero[i][j] != null) {
+                    contadorBloques++;
+                }
+            }
+        }
+        return contadorBloques;
+    }
+
+    public int contarBloquesPorTipo(TipoBloque tipo){
+        int numBloquesTipo=0;
+        if (tipo == null){
+            throw new IllegalArgumentException("El tipo no existe");
+        }
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (tablero[i][j] != null && tablero[i][j].getTipo()==tipo) {
+                    numBloquesTipo++;
+                }
+            }
+        }
+        return numBloquesTipo;
+
+    }
+
+    public boolean estaVacia(){
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (tablero[i][j] != null ) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
