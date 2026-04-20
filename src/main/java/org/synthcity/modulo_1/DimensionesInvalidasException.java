2600c5a7ed6eb5f9030b8cddb4373a92b6b908e5
Nicodawdev <napc67@educa.madrid.org>
2026-04-21 00:55:55 +0200

    Implementadas Excepciones DimensionesInvalidasException, y ExpansionCiudadException . Implementada Clase ResumenEstructuralCiudad

---
+++ src/main/java/org/synthcity/modulo_1/DimensionesInvalidasException.java
@@ -0,0 +1,7 @@
+package org.synthcity.modulo_1;
+
+public class DimensionesInvalidasException extends IllegalArgumentException {
+    public DimensionesInvalidasException(String mensaje){
+        super(mensaje);
+    }
+}
