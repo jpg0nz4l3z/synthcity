bafe6162d148db4a0a2e1e04cb4c52a69a70b1d9
Nicodawdev <napc67@educa.madrid.org>
2026-04-21 01:34:32 +0200

    M4: Ampliaciones PresentadorCiudad y SalidaTexto

--- src/main/java/org/synthcity/modulo_4/SalidaTexto.java
+++ src/main/java/org/synthcity/modulo_4/SalidaTexto.java
@@ -1,10 +1,18 @@
 package org.synthcity.modulo_4;
 
-public final class SalidaTexto {
 
-    private final String contenido;
+public class SalidaTexto {
+    private String titulo;
+    private String contenido;
+    private String nombreCiudad;
+    private String fechaGeneracion;
 
     public SalidaTexto(String contenido) {
+        this("Salida SynthCity", contenido, null, null);
+    }
+
+    public SalidaTexto(String titulo, String contenido, String nombreCiudad, String fechaGeneracion) {
+        this.titulo = titulo;
         if (contenido == null) {
             throw new FormatoSalidaException("El contenido no puede ser null.");
         }
@@ -12,10 +20,25 @@ public final class SalidaTexto {
             throw new FormatoSalidaException("El contenido no puede estar vacio.");
         }
         this.contenido = contenido;
+        this.nombreCiudad = nombreCiudad;
+        this.fechaGeneracion = fechaGeneracion;
+    }
+
+    // Getters
+    public String getTitulo() {
+        return titulo;
     }
 
     public String getContenido() {
         return contenido;
+    }
+
+    public String getNombreCiudad() {
+        return nombreCiudad;
+    }
+
+    public String getFechaGeneracion() {
+        return fechaGeneracion;
     }
 
     @Override
