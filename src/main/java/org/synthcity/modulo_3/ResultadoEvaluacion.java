ee9e0636729f293e1fc2a160d7cc51a237aac2d1
Maritza <maritzagonza21@gmail.com>
2026-04-27 15:04:08 +0200

    Se añade fueExpandida a ResultadoEvaluacion

--- src/main/java/org/synthcity/modulo_3/ResultadoEvaluacion.java
+++ src/main/java/org/synthcity/modulo_3/ResultadoEvaluacion.java
@@ -107,6 +107,10 @@ public class ResultadoEvaluacion {
                 || nivelEvaluacion == NivelEvaluacion.OPTIMO;
     }
 
+    public boolean fueExpandida() {
+        return false;
+    }
+
     @Override
     public String toString() {
         return "Ciudad: " + nombreCiudad +
