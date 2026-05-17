fc0adf187c459b09a1c14eb0e69f39526ab074a9
Diego <diegoalejandrorc22@gmail.com>
2026-05-17 21:33:28 +0200

    Implementación de clase global de configuración ConfigRutas.java en la ruta src/main/java/com/synthcity/modulo4/config

---
+++ src/main/java/com/synthcity/modulo4/config/ConfigRutas.java
@@ -0,0 +1,8 @@
+package com.synthcity.modulo4.config;
+
+public class ConfigRutas {
+    // Rutas estáticas obligatorias del enunciado del Sprint 4
+    public static final String RUTA_DATASET = "data/dataset/dataset.csv";
+    public static final String RUTA_MODELO = "data/modelo/modelo_weka.model";
+    public static final String RUTA_ESTRUCTURA = "data/modelo/estructura_instances.model";
+}
